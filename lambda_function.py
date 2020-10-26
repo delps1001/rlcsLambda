a7f745e6ce37b9e988555c0d7376ff0e3b52ff32
Dalton DelPiano <dalton.delpiano@ibm.com>
2020-10-26 16:40:49 -0400

    fix: add OCE and SAM

--- lambda_function.py
+++ lambda_function.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 import aiohttp
 import html
 import asyncio
+from enum import Enum
 
 URL = "https://liquipedia.net/rocketleague/api.php"
 
 PARAMS_NA = {
     'action': "parse",
-    'pageid': 89823,
+    'pageid': 89821,
     'prop': 'text',
     'section': 1,
     'format': "json"
@@ -16,29 +17,57 @@ PARAMS_NA = {
 
 PARAMS_EU = {
     'action': "parse",
-    'pageid': 89816,
+    'pageid': 89815,
+    'prop': 'text',
+    'section': 1,
+    'format': "json"
+}
+
+PARAMS_OCE = {
+    'action': "parse",
+    'pageid': 91700,
+    'prop': 'text',
+    'section': 1,
+    'format': "json"
+}
+
+PARAMS_SAM = {
+    'action': "parse",
+    'pageid': 91528,
     'prop': 'text',
     'section': 1,
     'format': "json"
 }
 
 
-async def get_table(url, params):
+class Region(Enum):
+    EU = 'EU'
+    NA = 'NA'
+    SAM = 'SAM'
+    OCE = 'OCE'
+
+
+async def get_table(url, params, region):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=params) as response:
             print('getting table for ' + str(params))
             data = await response.json()
             html_unescaped = html.unescape(data['parse']['text']['*'])
             parsed_html = BeautifulSoup(html_unescaped, features="html.parser")
-            results = filter_results(parsed_html)
+            results = filter_results(parsed_html, region)
             team_entries = gather_team_entries(results)
             return team_entries
 
 
-def filter_results(parsed_html):
-    results = filter(lambda x: str(x.get('data-toggle-area-content')) == '6', parsed_html.find_all('tr'))
-    results = filter(lambda x: x.find('span', attrs={'class': 'team-template-team-standard'}) is not None,
-                     results)
+def filter_results(parsed_html, region):
+    data_toggle_area_content = '6'
+    if region in [Region.NA, Region.OCE]:
+        data_toggle_area_content = '7'
+    elif region in [Region.SAM]:
+        data_toggle_area_content = '4'
+    results = filter(lambda x: str(x.get('data-toggle-area-content')) == data_toggle_area_content,
+                     parsed_html.find_all('tr'))
+    results = filter(lambda x: x.find('span', attrs={'class': 'team-template-team-standard'}) is not None, results)
     return results
 
 
@@ -52,8 +81,10 @@ def gather_team_entries(results):
 
 
 async def get_region_tables():
-    return await asyncio.gather(asyncio.create_task(get_table(URL, PARAMS_NA)),
-                                asyncio.create_task(get_table(URL, PARAMS_EU)))
+    return await asyncio.gather(asyncio.create_task(get_table(URL, PARAMS_NA, Region.NA)),
+                                asyncio.create_task(get_table(URL, PARAMS_EU, Region.EU)),
+                                asyncio.create_task(get_table(URL, PARAMS_OCE, Region.OCE)),
+                                asyncio.create_task(get_table(URL, PARAMS_SAM, Region.SAM)))
 
 
 tables = {}
@@ -61,9 +92,11 @@ tables = {}
 
 def get_region_tables_sync():
     if 'NA' not in tables or 'EU' not in tables:
-        (na_table, eu_table) = asyncio.get_event_loop().run_until_complete(get_region_tables())
+        (na_table, eu_table, oce_table, sa_table) = asyncio.get_event_loop().run_until_complete(get_region_tables())
         tables['NA'] = na_table
         tables['EU'] = eu_table
+        tables['OCE'] = oce_table
+        tables['SAM'] = sa_table
 
 
 def lambda_handler(event, context):
